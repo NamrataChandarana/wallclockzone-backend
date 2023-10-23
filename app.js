10e193b5cc96009d2fb31738a3c3f6e3369e0e00
Namrata Chandrana <namratachandarana20@gmail.com>
2023-10-23 22:29:43 +0530

    cors setup

--- app.js
+++ app.js
@@ -37,9 +37,11 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Origin: http://localhost:3000");
+  res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers: Content-Type, Authorization, X-Auth-Token, Origin"
+  );
   next();
 });
 
