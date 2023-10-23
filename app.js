81a1663002ac5fa148bb8dd68ec47bbc488fc651
Namrata Chandrana <namratachandarana20@gmail.com>
2023-10-24 00:12:13 +0530

    core setup

--- app.js
+++ app.js
@@ -36,14 +36,15 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin: http://localhost:3000");
-  res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers: Content-Type, Authorization, X-Auth-Token, Origin"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin: http://localhost:3000");
+//   res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers: Content-Type, Authorization, X-Auth-Token, Origin"
+//   );
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 //middelwares
 app.use(express.urlencoded({ extended: true }));
@@ -58,7 +59,7 @@ app.use(errorMiddleware);
 connectDB();
 
 const listen = app.listen(process.env.PORT, (req, res) => {
-  console.log("done");
+  console.log(`done and ${process.env.FRONTEND_URL}`);
 });
 
 //unhandled error (server error)
